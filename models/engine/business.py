54934dc295aae20d3daf88d37daa3cf57d01af45
Bonface Murangiri <bonnyrangi95@gmail.com>
2024-02-15 11:36:57 +0300

    Create business

---
+++ models/engine/business.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+""" holds class Business"""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
+from sqlalchemy.orm import relationship
+
+                
+class Business(BaseModel, Base):
+    """Representation of business """
+    if models.storage_t == 'db':
+        __tablename__ = 'businesses'
+        category_id = Column(String(128), ForeignKey('categories.id'), nullable=False)
+	location_id = Column(String(128), ForeignKey('locations.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024), nullable=True)
+        reviews = relationship("Review",
+                               backref="business",
+                               cascade="all, delete, delete-orphan")
+    else:
+        category_id = ""
+        location_id = ""
+        user_id = ""
+        description = ""
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Place"""
+        super().__init__(*args, **kwargs)
+
+    if models.storage_t != 'db':
+        @property
+        def reviews(self):
+            """getter attribute returns the list of Review instances"""
+            from models.review import Review
+            review_list = []
+            all_reviews = models.storage.all(Review)
+            for review in all_reviews.values():
+                if review.place_id == self.id:
+                    review_list.append(review)
+            return review_list
