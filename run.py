c7c87ed963f0a51ba2e14f4f5cbbe60cec21310c
Bonface Murangiri <bonnyrangi95@gmail.com>
2024-03-22 12:40:37 +0300

    Start celery and flask application

--- run.py
+++ run.py
@@ -5,7 +5,9 @@ from app import create_app
 import os
 
 config_name = os.getenv('APP_SETTINGS')
-app = create_app(config_name)
+app, celery = create_app(config_name)
+app.app_context().push()
+
 
 if __name__ == '__main__':
-        app.run(host='0.0.0.0',port=5000, debug=True)
+    app.run(host='0.0.0.0',port=5000, debug=True)
