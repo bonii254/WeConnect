0826bdc185842f6d8015751c0ffd2d26a350fe3b
Bonface Murangiri <bonnyrangi95@gmail.com>
2024-03-22 12:46:21 +0300

    Instanciate sqlalchemy, mail, bcrypt and define make_celery function

--- app/extensions.py
+++ app/extensions.py
@@ -1,9 +1,25 @@
-#!/usr/bin/python3
-"""instantiate mail and sqlalchemy"""
-
+#!/usr/bin/env python3
+"""instantiate mail, celery, Bcrypt and sqlalchemy"""
+import os
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
-
+from flask_bcrypt import Bcrypt
+from celery import Celery
 
 db = SQLAlchemy()
 mail = Mail()
+bcrypt = Bcrypt()
+
+
+def make_celery(app):
+    celery = Celery(app.import_name)
+    celery.conf.update(app.config["CELERY_CONFIG"])
+
+    class ContextTask(celery.Task):
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return self.run(*args, **kwargs)
+
+    celery.Task = ContextTask
+    return celery
