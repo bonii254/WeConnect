959d33e6dfa6650933f44ce542127a19b7d1a410
Bonface Murangiri <bonnyrangi95@gmail.com>
2024-03-27 22:46:10 +0300

    Improved code readability and consistency.

--- app/api/v2/review.py
+++ app/api/v2/review.py
@@ -28,16 +28,20 @@ def create_review(current_user, businessId):
     errors = validator.errors
     if errors:
         return jsonify({"Errors": errors}), 401
-    review_info['user_id'] = current_user.id
-    review_info['business_id'] = business.id
-    new_review = Review(**review_info)
+    user_id = current_user.id
+    business_id = business.id
+    title = review_info['title']
+    review = review_info['review']
+    new_review = Review(
+        user_id=user_id, business_id=business_id, title=title, review=review)
     db.session.add(new_review)
     db.session.commit()
     return jsonify({
         "message": "Review sent",
         "Review": {
-            "id": new_review.id, "title": new_review.title,
-            "body": new_review.body, "user_id": new_review.user_id,
+            "id": new_review.id, "business_name": business.name,
+            "title": new_review.title,
+            "review": new_review.review, "user_id": new_review.user_id,
             "business_id": new_review.business_id}}), 201
 
 
@@ -49,21 +53,24 @@ def get_business_reviews(businessId):
     if not business:
         return jsonify({"message": "Business not found"}), 404
     page = request.args.get('page', 1, type=int)
-    limit = request.args.get('limit', 10, type=int)
-    reviews = Review.query.filter_by(
+    per_page = request.args.get('limit', 10, type=int)
+    my_reviews = Review.query.filter_by(
         business_id=businessId).order_by(Review.created_at.desc()).paginate(
-            page, limit, False).items
+            page=page, per_page=per_page, error_out=False)
+    reviews = my_reviews.items
     if not reviews:
         return jsonify({"message": "No Reviews for this business"})
     return jsonify({
-        "message": f"reviews for {business}",
+        "message": f"reviews for {business.name}",
         "Details": [
             {
                 "id": review.id, "title": review.title,
-                "body": review.body, "user_id": review.user_id,
+                "review": review.review, "user_id": review.user_id,
                 "business_id": review.business_id
-                } for review in reviews]
-     }), 200
+                } for review in reviews],
+        "per_page": my_reviews.per_page, "page": my_reviews.page,
+        "total": my_reviews.total, "pages": my_reviews.pages,
+    }), 200
 
 
 @review.route(
@@ -82,13 +89,27 @@ def update_business_reviews(current_user, reviewId):
         return jsonify({"Errors": errors})
     if review.user == current_user:
         review.title = review_info['title']
-        review.body = review_info['body']
+        review.review = review_info['review']
         db.session.commit()
         return jsonify({
             "message": "review updated sucessfully",
             "Details": {
                 "id": review.id, "title": review.title,
-                "body": review.body, "user_id": review.user_id,
+                "review": review.review, "user_id": review.user_id,
                 "business_id": review.business_id
             }}), 200
     return jsonify({"Error": "you can only update your review"})
+
+
+@review.route(
+    'businesses/reviews/<reviewId>', methods=['DELETE'], strict_slashes=False)
+@login_required
+def delete_business_review(current_user, reviewId):
+    review = Review.query.filter_by(id=reviewId).first()
+    if review:
+        if review.user_id == current_user.id:
+            db.session.delete(review)
+            db.session.commit()
+            return jsonify({"message": "review deleted"}), 200
+        return jsonify({"error": "you can only delete your own review"}), 403
+    return jsonify({"error": "review not found"}), 404
